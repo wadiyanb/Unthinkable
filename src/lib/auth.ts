924ee17aed4c7f1c9869a29835774ff681f589f4
wadiyanb <arzaanswadia@gmail.com>
2026-08-24 22:29:08 +0530

    fix auth secret env var for netlify

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -66,5 +66,5 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
 })
